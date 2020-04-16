0fd5beb7baa5c3016fe153d2be194fc86bef8315
Tom Wilkus <twilkus89@gmail.com>
2020-04-16 17:33:21 -0500

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.SetSoundFolderButton = new System.Windows.Forms.Button();
+            this.KillSoundButton = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.labelFolderPath = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "(Empty)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "(Empty)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "(Empty)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(119, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 60);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "(Empty)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button_MouseUp);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // SetSoundFolderButton
+            // 
+            this.SetSoundFolderButton.Location = new System.Drawing.Point(13, 277);
+            this.SetSoundFolderButton.Name = "SetSoundFolderButton";
+            this.SetSoundFolderButton.Size = new System.Drawing.Size(100, 34);
+            this.SetSoundFolderButton.TabIndex = 17;
+            this.SetSoundFolderButton.Text = "Set Sound Folder Path";
+            this.SetSoundFolderButton.UseVisualStyleBackColor = true;
+            this.SetSoundFolderButton.Click += new System.EventHandler(this.SetSoundFolderButton_Click);
+            // 
+            // KillSoundButton
+            // 
+            this.KillSoundButton.BackColor = System.Drawing.Color.Red;
+            this.KillSoundButton.Location = new System.Drawing.Point(331, 277);
+            this.KillSoundButton.Name = "KillSoundButton";
+            this.KillSoundButton.Size = new System.Drawing.Size(100, 34);
+            this.KillSoundButton.TabIndex = 18;
+            this.KillSoundButton.Text = "Stop All Sounds";
+            this.KillSoundButton.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(119, 211);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 60);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "(Empty)";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(225, 211);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 60);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "(Empty)";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(331, 211);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 60);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "(Empty)";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(13, 211);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 60);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "(Empty)";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(119, 79);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 60);
+            this.button9.TabIndex = 26;
+            this.button9.Text = "(Empty)";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(225, 79);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 60);
+            this.button10.TabIndex = 25;
+            this.button10.Text = "(Empty)";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(331, 79);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 60);
+            this.button11.TabIndex = 24;
+            this.button11.Text = "(Empty)";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(13, 79);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(100, 60);
+            this.button12.TabIndex = 23;
+            this.button12.Text = "(Empty)";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(119, 145);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(100, 60);
+            this.button13.TabIndex = 30;
+            this.button13.Text = "(Empty)";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(225, 145);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(100, 60);
+            this.button14.TabIndex = 29;
+            this.button14.Text = "(Empty)";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(331, 145);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(100, 60);
+            this.button15.TabIndex = 28;
+            this.button15.Text = "(Empty)";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(13, 145);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(100, 60);
+            this.button16.TabIndex = 27;
+            this.button16.Text = "(Empty)";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // labelFolderPath
+            // 
+            this.labelFolderPath.AutoSize = true;
+            this.labelFolderPath.Location = new System.Drawing.Point(119, 288);
+            this.labelFolderPath.Name = "labelFolderPath";
+            this.labelFolderPath.Size = new System.Drawing.Size(91, 13);
+            this.labelFolderPath.TabIndex = 31;
+            this.labelFolderPath.Text = "No Path Selected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(444, 324);
+            this.Controls.Add(this.labelFolderPath);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.KillSoundButton);
+            this.Controls.Add(this.SetSoundFolderButton);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Soundboard";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button KillSoundButton;
+        private System.Windows.Forms.Button SetSoundFolderButton;
+        private System.Windows.Forms.Label labelFolderPath;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using System.IO;
+using System.Globalization;
+
+namespace WindowsFormsApp1
+{
+    static class Globals
+    {
+        public static string soundFolderPath;
+        public static List<string> soundFiles;
+        public static ContextMenu cm;
+        public static string buttonText = null;
+
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void SetSoundFolderButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.UserProfile;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                labelFolderPath.Text = folderBrowserDialog1.SelectedPath;
+                Globals.soundFolderPath = labelFolderPath.Text;
+            }
+            
+           
+        }
+
+        private void button_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            Control pressedButton = sender as Control;
+
+            if(e.Button == MouseButtons.Right)
+            {
+                setFileList();
+                Globals.soundFiles.Sort();
+                Globals.cm = new ContextMenu();
+                foreach (var item in Globals.soundFiles)
+                {
+                    Globals.cm.MenuItems.Add(new MenuItem(item, MenuItem_Click));
+                }
+
+                Globals.cm.Show(pressedButton, new Point(pressedButton.Height / 2, pressedButton.Width / 2));
+
+
+                MessageBox.Show("1");
+
+                pressedButton.Text = Globals.buttonText;
+
+            }
+            else
+            {
+                WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+                player.URL = Globals.soundFolderPath + "\\" + Globals.buttonText + "\\" + ".mp3";
+                player.controls.play();
+            }
+        }
+
+        private void setFileList()
+        {
+            Globals.soundFiles = new List<string>();
+
+            String[] soundFilesWithPath = Directory.GetFiles(Globals.soundFolderPath,"*.mp3");
+            TextInfo ti = new CultureInfo("en-US", false).TextInfo;
+
+            foreach (var item in soundFilesWithPath)
+            {
+                var sections = item.Split('\\');
+                var fileNameWithExt = (sections[sections.Length - 1]).Split('.');
+                var fileName = fileNameWithExt[0];
+
+                fileName = ti.ToTitleCase(fileName);
+
+                Globals.soundFiles.Add(fileName);
+            }
+        }
+
+        private void MenuItem_Click(Object sender, System.EventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+
+            Globals.buttonText = item.Text;
+
+        }
+    }
+}
